ad60158f108c357d1c2e7db034af4f755ebae22e
HorseNoRemorse <48712943+Nastydissonance@users.noreply.github.com>
2025-08-15 19:30:54 +0300

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Бать, ты ваще огонь 🔥';
+});
+
